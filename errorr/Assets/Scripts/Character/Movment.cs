cb395125460f2abc0af9da11ccd5df8c128700e9
vntsz <124453802+mootaz23@users.noreply.github.com>
2023-09-29 15:02:25 +0100

    g

--- errorr/Assets/Scripts/Character/Movment.cs
+++ errorr/Assets/Scripts/Character/Movment.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+ 
+public class Movment : NetworkBehaviour
+    { 
+        public float Speed = 15f;
+        public FixedJoystick fixedJoystick;
+        public Animator anim;
+        private void Update() {
+            transform.Translate((Input.GetAxis("Horizontal")+ fixedJoystick.Horizontal) * Speed * Time.deltaTime, (Input.GetAxis("Vertical") + fixedJoystick.Vertical)* Speed  * Time.deltaTime, 0f);
+            
+            anim.SetFloat("Horizontal",(Input.GetAxis("Horizontal")+ fixedJoystick.Horizontal));
+            anim.SetFloat("Vertical",(Input.GetAxis("Vertical") + fixedJoystick.Vertical));
 
-public class Movment : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+            Vector3 characterScale = transform.localScale;
+            if(Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -1;
+        }
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 1;
+        }
+        transform.localScale = characterScale;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
