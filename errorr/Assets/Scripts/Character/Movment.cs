d60e797e67b248da9b5f44cb6f803c27b4b802ce
Torbaga <78365108+Fedi15@users.noreply.github.com>
2023-09-30 16:03:36 +0100

    0

--- errorr/Assets/Scripts/Character/Movment.cs
+++ errorr/Assets/Scripts/Character/Movment.cs
@@ -8,24 +8,31 @@ public class Movment : NetworkBehaviour
         public float Speed = 15f;
         public FixedJoystick fixedJoystick;
         public Animator anim;
+        public Rigidbody2D rb;
+        private float inputX;
+        private float inputY;
         private void Update() {
-            float inputX = Input.GetAxis("Horizontal")+ fixedJoystick.Horizontal;
-            float inputY = Input.GetAxis("Vertical") + fixedJoystick.Vertical;
+            inputX = Input.GetAxis("Horizontal")+ fixedJoystick.Horizontal;
+            inputY = Input.GetAxis("Vertical") + fixedJoystick.Vertical;
             
-            transform.Translate(inputX * Speed * Time.deltaTime, inputY* Speed  * Time.deltaTime, 0f);
-            
+            //transform.Translate(inputX * Speed * Time.deltaTime, inputY* Speed  * Time.deltaTime, 0f);
             anim.SetFloat("Horizontal",inputX);
             anim.SetFloat("Vertical",inputY);
 
             Vector3 characterScale = transform.localScale;
             if(inputX < 0)
             {
-                characterScale.x = 1 ;
+                characterScale.x = -1 ;
             }
             if(inputX > 0)
             {
-                characterScale.x = -1;
+                characterScale.x = 1;
             }
             transform.localScale = characterScale;
+        }
+        private void LateUpdate() 
+        {
+            rb.velocity = new Vector2(inputX * Speed, inputY * Speed);
+
         }
     }
